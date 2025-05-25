6f9084c38f071dd52c6aa7fd72de2a6d4245428f
rishabh_nagar_hyperverge <rishabh.nagar@hyperverge.co>
2025-05-25 16:26:59 +0530

    init commit

---
+++ src/data/seo.js
@@ -0,0 +1,23 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Hi I am Rishabh Nagar, A backend engineer with a strong foundation in building scalable, secure, and high-availability systems using Node.js and modern frameworks. Experienced in developing production-grade APIs, automation tools, and cloud-native services handling millions of API calls monthly. I take pride in writing clean, maintainable code that adheres to best practices and industry standards. Passionate about solving complex problems, continuously learning, and contributing to impactful products. Always eager to embrace new challenges and drive innovation through technology.",
+		keywords: ["Rishabh", "Rishabh N", "Rishabh Nagar"],
+	},
+	{
+		page: "works",
+		description:
+			"Hi I am Rishabh Nagar, A backend engineer with a strong foundation in building scalable, secure, and high-availability systems using Node.js and modern frameworks. Experienced in developing production-grade APIs, automation tools, and cloud-native services handling millions of API calls monthly. I take pride in writing clean, maintainable code that adheres to best practices and industry standards. Passionate about solving complex problems, continuously learning, and contributing to impactful products. Always eager to embrace new challenges and drive innovation through technology.",
+		keywords: ["Rishabh", "Rishabh N", "Rishabh Nagar"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"Hi I am Rishabh Nagar, A backend engineer with a strong foundation in building scalable, secure, and high-availability systems using Node.js and modern frameworks. Experienced in developing production-grade APIs, automation tools, and cloud-native services handling millions of API calls monthly. I take pride in writing clean, maintainable code that adheres to best practices and industry standards. Passionate about solving complex problems, continuously learning, and contributing to impactful products. Always eager to embrace new challenges and drive innovation through technology.",
+		keywords: ["Rishabh", "Rishabh N", "Rishabh Nagar"],
+	}
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,71 @@
+const INFO = {
+	main: {
+		title: "Rishabh Nagar",
+		name: "Rishabh Nagar",
+		email: "mail@rishabhnagar11061999@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-Stack Engineer | Backend Specialist | Passionate Problem Solver",
+		description:
+			"Hi I am Rishabh Nagar, A backend engineer with a strong foundation in building scalable, secure, and high-availability systems using Node.js and modern frameworks. Experienced in developing production-grade APIs, automation tools, and cloud-native services handling millions of API calls monthly. I take pride in writing clean, maintainable code that adheres to best practices and industry standards. Passionate about solving complex problems, continuously learning, and contributing to impactful products. Always eager to embrace new challenges and drive innovation through technology."	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
